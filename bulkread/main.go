89e188f3f02acd6c4de063b6410d3e79d95c7a18
fangzhou <akafangzhou@gmail.com>
2014-12-09 18:57:19 +0800

    add bulkread for testing GetDeviceWithVidPid

---
+++ bulkread/main.go
@@ -0,0 +1,66 @@
+// Copyright 2013 Google Inc.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// rawread attempts to read from the specified USB device.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/JohnFarmer/gousb/usb"
+	//"github.com/JohnFarmer/gousb/usbid"
+)
+
+var (
+	device_vid_pid   = flag.String(
+		"device",
+		"0483:5741",
+		"Device to which to connect")
+	config   = flag.Int("config", 1, "Endpoint to which to connect")
+	iface    = flag.Int("interface", 0, "Endpoint to which to connect")
+	setup    = flag.Int("setup", 0, "Endpoint to which to connect")
+	endpoint = flag.Int("endpoint", 1, "Endpoint to which to connect")
+	debug    = flag.Int("debug", 3, "Debug level for libusb")
+)
+
+func main() {
+	flag.Parse()
+
+	// Only one context should be needed for an application.
+	// It should always be closed.
+	ctx := usb.NewContext()
+	defer ctx.Close()
+	ctx.Debug(*debug)
+
+	log.Printf("Scanning for device %q...", *device_vid_pid)
+
+	// Get device by VID:PID string like "xxxx:xxxx"
+	dev, _ := ctx.GetDeviceWithVidPid(*device_vid_pid)
+	defer dev.Close()
+
+	log.Printf("Connecting to endpoint...")
+	log.Printf("- %#v", dev.Descriptor)
+	ep, err := dev.OpenEndpoint(uint8(*config), uint8(*iface), uint8(*setup), uint8(*endpoint)|uint8(usb.ENDPOINT_DIR_IN))
+	if err != nil {
+		log.Fatalf("open: %s", err)
+	}
+
+	buf := make([]byte, 64)
+	ep.Read(buf)
+
+	fmt.Println(string(buf[:]))
+	fmt.Printf("%c\n", buf)
+}
